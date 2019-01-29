7bc039f59d4034126d342ab544adc049eae8edaa
YaOliveira <magicsunshine_ya@msn.com>
2019-01-29 11:07:01 -0200

    envio parcial

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,27 @@
 window.cipher = {
+
+  
   // ... 
 };
 
-function Enviar1()
-{
-document.getElementById("teste").innerHTML = 'aeeee';
+let text;
+let offset;
+
+function(word, desloc){
+
 }
 
+// encode
+
+// decode
+
+/* 
+Deve utilizar:
+-charCodeAt()
+
+-String.fromCharCode()
+
+- AscII (a a z - 65 a 90)
+
+((CodigoDaLetraASC - cod1letra + desloc) % tamanhodoalfabeto)+codigo1letra
+*/
