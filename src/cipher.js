23f05d06a7925f7eff626d62cb139492f20e5764
YaOliveira <magicsunshine_ya@msn.com>
2019-01-30 14:06:12 -0200

    melhorias no js e chamar função e value

--- src/cipher.js
+++ src/cipher.js
@@ -4,22 +4,71 @@
   // ... 
 };
 */
+// function paloma (){
+//   console.log("paloma");
+// }
+// paloma()
 
+console.log ("Passo 1")
 let cipher ={};
 
 function btnEncod(){
-  console.log ("inicia função btn")
-  let writtensentence = document.getElementById("writePhrase");
-  let offset = document.getElementById("displacement");
-  let result = cipher.encode (offset,writtensentence);
+        console.log("inicia função btn")
+    let writtensentence = document.getElementById("writePhrase").value;
+    let offset = document.getElementById("displacement").value;
+    let result = cipher.encode(offset,writtensentence);
   document.getElementById("textResult").innerHTML = result;
-  console.log(btnEncod);
-  console.log(writtensentence);
-  console.log(offset);
-  console.log(result);
-  console.log("finaliza função btn");
+        console.log(btnEncod);
+        console.log(writtensentence);
+        console.log(offset);
+        console.log(result);
+        console.log("finaliza função btn");
 
 }
+
+
+console.log("Passo 2")
+cipher.encode = (offset,string) => {
+       let resultEncode ="";
+// tranforma letra em asc
+  for (let i = 0; i < string.length; i++) {
+       let codAscii = string.charCodeAt(i);
+       
+    if (codAscii >= 65 && codAscii <= 90) {
+      console.log("passou pela letra maiúscula");
+      let result = ((codAscii - 65 + parseInt(offset)) % 26 + 65);
+      let resultLetterTransform = String.fromCharCode(result);
+      resultEncode = resultEncode + resultLetterTransform;
+        console.log(resultLetter);
+        console.log(resultEncode);
+        console.log(parseInt);
+        console.log(String);
+        console.log(codAscii);
+      //resultEncode += String.fromCharCode((codAscii - 65 + offset) % 26 + 65);
+         
+    } else if (codAscii >= 97 && codAscii <= 122) {
+
+      let result = ((codAscii - 97 + parseInt(offset)) % 26 + 97);
+      let resultLetterTransform = String.fromCharCode(result);
+      resultEncode = resultEncode + resultLetterTransform;
+      //resultEncode += String.fromCharCode((codAscii - 97 + parseInt(offset)) % 26 + 97);
+        console.log("passou pela letra minúscula");
+    } else {
+      resultEncode = resultEncode + string[i];
+      //   resultEncode += String.fromCharCode((resultEncode));
+        console.log(string);
+        console.log(resultEncode);
+        console.log("passou pela concatenação");
+    }
+  }
+  return resultEncode;
+  
+}
+
+
+
+//(variavelDaFrase.charcodeAt(i) - 65 + desloc) %26 - 65)
+
 /* --- explicação da vanessa em 29/01
 
 cipher = (offset,string) => {
@@ -36,34 +85,3 @@ cipher = (offset,string) => {
   }
 */
  
-cipher = (offset,string) => {
-
-for (let i = 0; i < string.length; i++) {
-    let convAscii = string.charCodeAt(i);
-
-    if (convAscii >= 65 && convAscii <= 90) {
-
-      let result = ((convAscii - 65 + parseInt(offset)) % 26 + 65);
-      let resultLetter = String.fromCharCode(result);
-      resultEncode = resultEncode + resultLetter;
-      console.log(resultLetter);
-      console.log(resultEncode);
-      console.log(parseInt);
-      console.log(String);
-      console.log(convAscii);
-      //resultEncode += String.fromCharCode((convAscii - 65 + offset) % 26 + 65);
-         
-    } else if (convAscii >= 97 && convAscii <= 122) {
-      resultEncode += String.fromCharCode((convAscii - 97 + parseInt(offset)) % 26 + 97);
-    } else {
-      resultEncode = resultEncode + string[i];
-      //   resultEncode += String.fromCharCode((resultEncode));
-    }
-  }
-  return resultEncode;
-},
-
-}
-(variavelDaFrase.charcodeAt(i) - 65 + desloc) %26 - 65)
-
-
--- src/tests.js
+++ src/tests.js
@@ -1,12 +1,14 @@
-let result = cipher("aBc",1);
-if(result === "bCd"){
+/*let resultEncode = cipher("aBc",1);
+if(resultEncode === "bCd"){
         console.log ("Deu certo");
 }else{
         console.log("Deu errado");
 }
+*/
 
-// Tests
-let result = 'veni, vidi, vici';
+/* Tests
+let resultEncode= 'veni, vidi, vici';
 for (let i = 0; i<26; i++) {
-  console.log(i+': '+caesar(text,i));
+  console.log(i+': '+cipher(string,i));
 }
+*/
